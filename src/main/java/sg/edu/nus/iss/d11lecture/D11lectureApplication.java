125f48cf02b4d0a6b7a01a0254a5f335a5db0057
Natasha <sn.natasha26@gmail.com>
2023-11-22 15:46:43 +0800

    update

--- src/main/java/sg/edu/nus/iss/d11lecture/D11lectureApplication.java
+++ src/main/java/sg/edu/nus/iss/d11lecture/D11lectureApplication.java
@@ -1,13 +1,45 @@
 package sg.edu.nus.iss.d11lecture;
 
+import java.util.Collections;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.DefaultApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @SpringBootApplication
 public class D11lectureApplication {
 
+// References:
+// ./mvnw spring-boot:run -Dspring-boot.run.arguments="--port=4000"
+// ./mvnw spring-boot:run -Dspring-boot.run.arguments="--port=5050"
+
 	public static void main(String[] args) {
-		SpringApplication.run(D11lectureApplication.class, args);
+		// SpringApplication.run(D11lectureApplication.class, args);
+
+		SpringApplication app = new SpringApplication(D11lectureApplication.class);
+
+		String port="8080";
+		ApplicationArguments argOptions = new DefaultApplicationArguments(args);
+
+		if (argOptions.containsOption("port")) {
+			port = argOptions.getOptionValues("port").get(0);			
+		}
+
+		app.setDefaultProperties(Collections.singletonMap("server.port", port));
+		app.run(args);
+
+		System.out.println("My first springboot application started...");
+	}
+
+	@Bean
+	public CommonsRequestLoggingFilter log() {
+		CommonsRequestLoggingFilter logger = new CommonsRequestLoggingFilter();
+		logger.setIncludeClientInfo(true);
+		logger.setIncludeQueryString(true);
+		return logger;
 	}
 
 }
---
+++ src/main/java/sg/edu/nus/iss/d11lecture/controller/EmployeeController.java
@@ -0,0 +1,29 @@
+package sg.edu.nus.iss.d11lecture.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import sg.edu.nus.iss.d11lecture.model.Employee;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+    
+
+    @GetMapping(path ="/list")
+    public List<Employee> getAllEmployees() {
+
+        List<Employee> employees = new ArrayList<>();
+        employees.add(new Employee("Ashley", "Chew", "ashchew@visa.com.sg"));
+        employees.add(new Employee("Alicia", "Ong", "aliciaong@visa.com.sg"));
+        employees.add(new Employee("Darien", "Lim", "darienlim@visa.com.sg"));
+        
+        
+        return employees;
+    }
+}
